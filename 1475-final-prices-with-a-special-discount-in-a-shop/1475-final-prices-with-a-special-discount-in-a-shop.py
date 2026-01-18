10362482ebbe1c556568237c3d98eb7199a2a669
Sheik <yasinnew72@gmail.com>
2026-01-18 14:26:13 +0530

    Time: 3 ms (50.28%), Space: 19.4 MB (6.6%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,13 +1,11 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        n = len(prices)
-        st = prices[:]
-        
-        for i in range(n):
-            j = i + 1
-            while j < n and prices[j] > prices[i]:
-                j += 1
-            if j < n:
-                st[i] = prices[i] - prices[j]
-        return st
+        st = []
+        for i , price in enumerate(prices):
+            while st and prices[st[-1]] >= price:
+                j = st.pop()
+                prices[j] -= price
+
+            st.append(i)
+        return prices
            
